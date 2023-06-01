89d3d0cdc9a198a47ce04e3f05813dca4c33337a
khalilelset <khoulioelset2000@gmail.com>
2023-06-01 20:33:39 +0300

    node moduls backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const mongoString = process.env.DATABASE_URL;
+//const routes = require('./route');
+var cors = require('cors');
+app.use(cors())
+//app.use('/api', routes)
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on('error', (error) => {
+    console.log(error)
+    })
+    database.once('connected', () => {
+    console.log('Database Connected');
+    })
+
+app.use(express.json());
+app.listen(4000, () => {
+console.log(`Server Started at \${4000}`)
+})
