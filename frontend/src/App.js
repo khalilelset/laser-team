aebcc1624d5a7d97b22010de6fd9410583a8ba5b
khalilelset <khoulioelset2000@gmail.com>
2023-06-07 09:52:18 +0300

    pompompiojiojiojpo

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import { Route, Routes } from "react-router-dom"
+import { useState } from "react"
+
+export default function App(){
+
+    return( 
+    
+    <div>
+        
+        </div>
+       
+    )
+}
