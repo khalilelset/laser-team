41376e11ec09a5aba68b873ef1c26f05947f8537
khalilelset <khoulioelset2000@gmail.com>
2023-06-07 17:48:15 +0300

    jwelcjnwcl

--- backend/Routes/store/StoreProduct.js
+++ backend/Routes/store/StoreProduct.js
@@ -11,4 +11,15 @@ router.get('/store/product', async (req, res) => {
     res.status(500).json({ message: error.message })
     }
     })
+
+ router.get('/store/product/getOne/:id', async (req, res) => {
+     try {
+     const data = await Product.findById(req.params.id);
+     res.json(data)
+     }
+     catch (error) {
+     res.status(500).json({ message: error.message })
+     }
+     })
+     
     module.exports = router;
