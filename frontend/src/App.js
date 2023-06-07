41cfce933540d5bb6abdef924e837baa794bccbc
khalilelset <khoulioelset2000@gmail.com>
2023-06-07 11:51:08 +0300

    start front end

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './stors/Rootreducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
