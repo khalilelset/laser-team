82619eb7298ad830200d399d7402e6cac46f2276
khaled <khaled.h2211@gmail.com>
2023-06-10 18:05:58 +0300

    routing CardDetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,23 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 import React from "react";
 
-import HomePage from "./Page/HomePage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import AllProductsCards from "./components/AllProductsCards/AllProductsCards";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./Page/HomePage/HomePage";
+import CardDetails from "./components/CardDetails/CardDetails";
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        
+        <Route path="/" element={ <HomePage/>} />
         <Route path="AllCards" element={<AllProductsCards />} />
+        <Route path="CardDetail" element={<CardDetails/>} />
       </Routes>
       <Footer />
     </div>
