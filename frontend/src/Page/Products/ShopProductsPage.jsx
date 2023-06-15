2085724f476fd9b35e3bdf9e85b4faeac770627e
khaled <khaled.h2211@gmail.com>
2023-06-15 18:46:24 +0300

    holaa

--- frontend/src/Page/Products/ShopProductsPage.jsx
+++ frontend/src/Page/Products/ShopProductsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+
 
 import { Col, Container, Row } from "react-bootstrap";
 
@@ -7,23 +7,78 @@ import Pagination from "../../components/Utility/Pagination";
 import SideFilter from "../../components/Utility/SideFilter";
 import SearchCountResult from "../../components/Utility/SearchCountResult";
 import CategoryHeader from "../../components/Categorie/CategoryHeader";
-import { useDispatch, useSelector } from "react-redux";
+import {  useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/actions/AllProductsActions";
+import Card from "../../components/Card/Card";
+import { useEffect, useState } from "react";
+
+
 
 const ShopProductsPage = () => {
+
+
+  const dispatch = useDispatch();
+  const productsData = useSelector((state) => state.products);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+  
+  useEffect(() => {
+    setProducts(productsData.products ?? []);
+  }, [productsData.products]);
+
+
+
+
+  
   return (
     <div style={{ minHeight: "670px" }}>
-      <CategoryHeader />
+
+
+      {/* <CategoryHeader /> */}
+      <div className="cat-header">
       <Container>
+        <Row>
+          <Col className="d-flex justify-content-start py-2 flex-wrap">
+            <div className="cat-text-header">All</div>
+            <div className="cat-text-header">Pride</div>
+            <div className="cat-text-header">Cloth</div>
+            <div className="cat-text-header">Soap</div>
+            <div className="cat-text-header">Wood</div>
+            <div className="cat-text-header">Accessor</div>
+            <div className="cat-text-header">Art</div>
+      
+          </Col>
+        </Row>
+      </Container>
+    </div>
+
+
+      <Container>
+
+
         <SearchCountResult title="500 search value" />
 
+
+
+
         <Row className="d-flex flex-row">
-          <Col sm="2" xs="2" md="1" className="d-flex">
-            <SideFilter />
-          </Col>
-          <Col sm="10" xs="10" md="11">
-            <CardProductsContainer title="" btntitle="" />
-          </Col>
+
+       
+
+            
+            <Row className="my-2 d-flex justify-content-between">
+   
+        {products.map((product) => (
+          // <ProductCard product={product} />
+          <Card product={product} />
+        ))}
+      </Row>
+
+
+
+          {/* </Col> */}
         </Row>
         <Pagination />
       </Container>
--- frontend/src/components/Categorie/CategoryHeader.jsx
+++ frontend/src/components/Categorie/CategoryHeader.jsx
@@ -7,16 +7,14 @@ const CategoryHeader = () => {
       <Container>
         <Row>
           <Col className="d-flex justify-content-start py-2 flex-wrap">
-            <div className="cat-text-header ">all</div>
-            <div className="cat-text-header">fruit</div>
-            <div className="cat-text-header">vetment</div>
-            <div className="cat-text-header"> honey</div>
-            <div className="cat-text-header">appel</div>
-            <div className="cat-text-header">asus</div>
-            <div className="cat-text-header">laptop</div>
-            <div className="cat-text-header">iphon</div>
-            <div className="cat-text-header">sumsung</div>
-            <div className="cat-text-header">more</div>
+            <div className="cat-text-header">All</div>
+            <div className="cat-text-header">Pride</div>
+            <div className="cat-text-header">Cloth</div>
+            <div className="cat-text-header">Soap</div>
+            <div className="cat-text-header">Wood</div>
+            <div className="cat-text-header">Accessor</div>
+            <div className="cat-text-header">Art</div>
+      
           </Col>
         </Row>
       </Container>
