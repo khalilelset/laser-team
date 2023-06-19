9648b4752e212c5304c04bb9fa2116242443d079
khalilelset <khoulioelset2000@gmail.com>
2023-06-19 14:11:39 +0300

    momomm

--- frontend/src/components/Admin/AdminAllProducts.js
+++ frontend/src/components/Admin/AdminAllProducts.js
@@ -1,23 +1,63 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row } from 'react-bootstrap'
 import AdminAllProductsCard from './AdminAllProductsCard'
+import { v4 as uuidv4 } from 'uuid';
 
 const AdminAllProducts = () => {
+
+    const [craftOwnerproduct, setCraftOwnerproduct] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const fetchCraftOwnerInfo = async () => {
+          try {
+            const emailL= window.localStorage.getItem("email");
+            const email=emailL.slice(1, -1);
+            
+            const response = await fetch(`http://localhost:4000/api/craftowner/getproduct/${email}`);
+            const data = await response.json();
+            setCraftOwnerproduct(data)
+            setIsLoading(false);
+            
+          } catch (error) {
+            
+          }
+          
+        };
+        
+        fetchCraftOwnerInfo();
+      }, []);
+
+
+      
+     const sketchItems=(data)=>{
+        return data.map((n)=> {
+            return (<AdminAllProductsCard _id={n._id} mainProductImage={n.mainProductImage} price={n.price}
+                 productAvailableQuantity={n.productAvailableQuantity} productCategory={n.productCategory} 
+                 productDescription={n.productDescription} productTitle={n.productTitle}
+                 productImage={n.productImage}
+
+
+                />)
+        })}
+
     return (
-        <div>
-            <div className='admin-content-text'>Manage all products</div>
+        <>             
+        {!isLoading ? ( 
+
+         <div>
+            <div className='admin-content-text'>Your Product posted</div>
             <Row className='justify-content-start'>
               
-                 <AdminAllProductsCard />
-                 <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-            </Row>
+            {sketchItems(craftOwnerproduct)}
 
-        </div>
+            </Row>
+           </div>
+       
+     ) : (<div className="loading-container">
+     <div className="loading-spinner"></div>
+     <p>Loading...</p> </div>)}
+ </>
     )
 }
 
